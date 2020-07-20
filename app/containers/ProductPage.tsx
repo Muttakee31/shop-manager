9203dc075c247fa40e261d632c619c309a54b06d
Muttakee31 <muttakee9@gmail.com>
2020-07-20 19:42:41 +0600

    emmni

--- app/containers/ProductPage.tsx
+++ app/containers/ProductPage.tsx
@@ -6,10 +6,10 @@ import Sidebar from './Sidebar';
 export default function ProductPage(): JSX.Element {
   return (
     <Grid container>
-      <Grid item xs={4}>
+      <Grid item xs={3}>
         <Sidebar />
       </Grid>
-      <Grid item xs={8}>
+      <Grid item xs={9}>
         <ProductList />
       </Grid>
     </Grid>
