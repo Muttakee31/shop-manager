74d45b53524f724fa9135b3424feb0c418c13840
Saad <muttakee9@gmail.com>
2020-10-06 21:43:59 +0600

    bumped version. checked transaction empty bug.

--- app/containers/HomePage.tsx
+++ app/containers/HomePage.tsx
@@ -400,20 +400,22 @@ export default function HomePage() {
           if (_err) {
             console.log(_err)
           } else {
-            let orderSum: number = 0;
-            let saleSum: number = 0;
-            let orderCount:number = 0;
-            instant.map(item => {
-              if (item.transaction_type === transactionType["order"]) {
-                orderSum += Number(item.order_cost);
-                orderCount++;
-              }
-              if (item.transaction_type === transactionType["supply"] ||
-                item.transaction_type === transactionType["other"]) saleSum += Number(item.order_cost);
-            });
-            setTotalSale(orderSum);
-            setTotalExpense(saleSum);
-            setOrderCount(orderCount);
+            if (instant !== undefined) {
+              let orderSum: number = 0;
+              let saleSum: number = 0;
+              let orderCount: number = 0;
+              instant.map(item => {
+                if (item.transaction_type === transactionType["order"]) {
+                  orderSum += Number(item.order_cost);
+                  orderCount++;
+                }
+                if (item.transaction_type === transactionType["supply"] ||
+                  item.transaction_type === transactionType["other"]) saleSum += Number(item.order_cost);
+              });
+              setTotalSale(orderSum);
+              setTotalExpense(saleSum);
+              setOrderCount(orderCount);
+            }
           }
         }
       );
