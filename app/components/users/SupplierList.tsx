0a7755eb85ab0bdbc3aa40af08364e711049fdd9
Muttakee31 <muttakee9@gmail.com>
2020-11-22 22:43:44 +0600

    delete modal added for customer and supplier.

--- app/components/users/SupplierList.tsx
+++ app/components/users/SupplierList.tsx
@@ -11,6 +11,13 @@ import { useHistory } from 'react-router';
 import VisibilityIcon from '@material-ui/icons/Visibility';
 import Sidebar from '../../containers/Sidebar';
 import * as dbpath from '../../constants/config';
+import EditIcon from '@material-ui/icons/Edit';
+import DeleteIcon from '@material-ui/icons/Delete';
+import Fade from '@material-ui/core/Fade';
+import Button from '@material-ui/core/Button';
+import Modal from '@material-ui/core/Modal';
+import { useSelector } from 'react-redux';
+import { isAuthenticated } from '../../features/auth/authSlice';
 
 const sqlite3 = require('sqlite3').verbose();
 
@@ -31,31 +38,99 @@ const useStylesToo = makeStyles({
     textDecoration: 'underline',
     textUnderlinePosition: 'under'
   },
+  deleteButton: {
+    background: '#ca263d',
+    marginRight: 15,
+    color: 'floralwhite',
+    '&:hover' : {
+      background: '#d7495d',
+      color: 'floralwhite',
+    },
+    '&:focus' : {
+      background: '#d94056',
+      color: 'floralwhite',
+    },
+  },
+  modal: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  paper: {
+    border: '2px solid #000',
+    background: '#232c39',
+    boxShadow: '3px 3px 20px #010101',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: 400,
+    height: 170,
+    margin: 15
+  },
 });
 
 export default function SupplierList(): JSX.Element {
   const classes = useStylesToo();
   const [supplierList, setSupplierList] = useState<Supplier[]>([]);
-  // const [productList, setProductList] = useState<Product[]>([]);
+
+
+  const authFlag= useSelector(isAuthenticated);
   const history = useHistory();
+  const [deleteModal, setDeleteModal] = useState(false);
+  const [toBeDeleted, setToBeDeleted] = useState(-1);
   // console.log('Connected to the shop database.');
   useEffect(() => {
     // add db.all function to get all suppliers
-    const db = new sqlite3.Database(dbpath.dbPath);
-    db.all(
-      'SELECT * FROM User where is_supplier = ?',
-      [1],
-      (_err: Error, instant: Supplier[]) => {
-        setSupplierList(instant);
-      }
-    );
-    db.close();
+    getSupplierList();
   }, []);
   const viewSupplier = (instant: Supplier) => {
     history.push({
       pathname: `/user-details/1/${instant.id}`,
     });
   };
+
+  const getSupplierList = () => {
+    try {
+      const db = new sqlite3.Database(dbpath.dbPath);
+      db.all(
+        'SELECT * FROM User where is_supplier = ?',
+        [1],
+        (_err: Error, instant: Customer[]) => {
+          setSupplierList(instant);
+        }
+      );
+      db.close();
+    }
+    catch (e) {
+      console.log(e);
+    }
+  }
+
+  const openDeleteSupplier = (instant: Transaction) => {
+    setDeleteModal(true);
+    setToBeDeleted(instant.id);
+  };
+
+  const deleteSupplier = () => {
+    try {
+      const db = new sqlite3.Database(dbpath.dbPath);
+      db.run(
+        'DELETE FROM User WHERE id = ?', [toBeDeleted],
+        (_err: Error) => {
+          if (_err) {
+            console.log(_err);
+          } else {
+            setDeleteModal(false);
+            getSupplierList();
+          }
+        }
+      );
+      db.close();
+    } catch (e) {
+      console.log(e);
+    }
+  }
   /*
   return (
     <Grid container>
@@ -113,6 +188,13 @@ export default function SupplierList(): JSX.Element {
                   </TableCell>
                   <TableCell align="center" className={classes.texts}>
                     <VisibilityIcon onClick={() => viewSupplier(row)} />
+                    {authFlag &&
+                    <>
+                      <EditIcon onClick={() => history.push(`/update-user/${row.id}`)}
+                                style={{ padding: '0 5px'}} />
+                      <DeleteIcon onClick={() => openDeleteSupplier(row)} />
+                    </>
+                    }
                   </TableCell>
                 </TableRow>
               ))}
@@ -120,6 +202,42 @@ export default function SupplierList(): JSX.Element {
           </Table>
         </TableContainer>
       </Grid>
+      <Modal
+        aria-labelledby="transition-modal-title"
+        aria-describedby="transition-modal-description"
+        className={classes.modal}
+        open={deleteModal}
+        onClose={()=> setDeleteModal(false)}
+        closeAfterTransition
+        BackdropProps={{
+          timeout: 500,
+        }}
+      >
+        <Fade in={deleteModal}>
+          <div className={classes.paper}>
+
+            <Grid style={{textAlign: 'center'}}>
+              <h3>
+                Are you sure?
+              </h3>
+            </Grid>
+
+            <Grid>
+              <Button
+                variant="contained"
+                color="primary"
+                fullWidth
+                className={classes.deleteButton}
+                onClick={() => {
+                  deleteSupplier();
+                }}
+              >
+                Delete
+              </Button>
+            </Grid>
+          </div>
+        </Fade>
+      </Modal>
     </Grid>
   );
 }
