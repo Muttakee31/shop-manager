46715182acf3c3210beed3c92e1db930f4dcb788
muttakee31 <muttakee9@gmail.com>
2021-05-01 01:55:23 +0600

    removed order/supply list.

--- app/components/users/UserDetails.tsx
+++ app/components/users/UserDetails.tsx
@@ -9,7 +9,6 @@ import TableBody from '@material-ui/core/TableBody';
 import { makeStyles } from '@material-ui/core/styles';
 import { useHistory, useLocation, useRouteMatch } from 'react-router';
 import dayjs from 'dayjs';
-import VisibilityIcon from '@material-ui/icons/Visibility';
 import * as dbpath from '../../constants/config';
 import { transactionType } from '../../constants/config';
 import BackButton from '../snippets/BackButton';
@@ -41,14 +40,6 @@ interface Transaction {
   due_amount: number | null;
   supply_id: number;
   timestamp: string;
-}
-
-interface Order {
-  id: number;
-  customer_name: string;
-  supplier_name: string;
-  timestamp: string;
-  total_cost: number;
 }
 
 const useStyles1 = makeStyles({
@@ -83,7 +74,6 @@ const emptyUser: User = {
 export default function UserDetails(): JSX.Element {
   const classes = useStyles1();
   const [user, setUser] = useState<User>(emptyUser);
-  const [orderList, setOrderList] = useState<Order[]>([]);
   const [transactionList, setTransactionList] = useState<Transaction[]>([]);
   const history = useHistory();
   const location = useLocation();
@@ -109,35 +99,7 @@ export default function UserDetails(): JSX.Element {
         }
       }
     );
-    if (type === '0') {
-      //console.log('customer');
-      db.all(
-        'SELECT * FROM Orders where customer=?',
-        [id],
-        (err: Error, instant: Order[]) => {
-          if (err) {
-            console.log(err);
-          } else {
-            setOrderList(instant);
-            //console.log(instant);
-          }
-        }
-      );
-    } else {
-      console.log('supplier');
-      db.all(
-        'SELECT * FROM Supply where supplier=?',
-        [id],
-        (err: Error, instant: Order[]) => {
-          if (err) {
-            console.log(err);
-          } else {
-            setOrderList(instant);
-            console.log(instant);
-          }
-        }
-      );
-    }
+
     db.all(
       'SELECT * FROM Transactions WHERE client = ?', [id],
       (_err: Error, instant: Transaction[]) => {
@@ -146,10 +108,6 @@ export default function UserDetails(): JSX.Element {
     );
     db.close();
   }, []);
-
-  const seeDetails = (id: number) => {
-    type === '0' ? history.push(`/order/${id}`) : history.push(`/supply/${id}`);
-  };
 
   const returnToPreviousPage = () => {
     history.replace({
@@ -214,52 +172,6 @@ export default function UserDetails(): JSX.Element {
         </Grid>
 
         <Grid className={classes.header}>
-          <h3>List of {type === '0' ? "Orders" : "Supplies"}</h3>
-        </Grid>
-
-        <Grid>
-          <TableContainer>
-            <Table aria-label="simple table">
-              <TableHead>
-                <TableRow>
-                  <TableCell className={classes.texts}>name</TableCell>
-                  <TableCell className={classes.texts}>Total cost</TableCell>
-                  <TableCell className={classes.texts}>Time of event</TableCell>
-                  <TableCell className={classes.texts} />
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {orderList.length === 0 ?
-                  <TableRow>
-                    <TableCell colSpan={4} align="left" className={classes.texts} style={{ textAlign: 'center' }}>
-                      No item
-                    </TableCell>
-                  </TableRow>
-                  :
-                  orderList.map((row) => (
-                  <TableRow key={row.id}>
-                    <TableCell align="left" className={classes.texts}>
-                      {type === '1' ? row.supplier_name : row.customer_name}
-                    </TableCell>
-                    <TableCell align="left" className={classes.texts}>
-                      {row.total_cost}
-                    </TableCell>
-                    <TableCell align="left" className={classes.texts}>
-                      {dayjs(row.timestamp.split('Z')[0]).format(
-                        'MMMM DD, YYYY [a]t hh:mm A'
-                      )}
-                    </TableCell>
-                    <TableCell align="left" className={classes.texts}>
-                      <VisibilityIcon onClick={() => seeDetails(row.id)} />
-                    </TableCell>
-                  </TableRow>
-                ))}
-              </TableBody>
-            </Table>
-          </TableContainer>
-        </Grid>
-
-        <Grid className={classes.header}>
           <h3>List of Transactions</h3>
         </Grid>
 
@@ -270,6 +182,7 @@ export default function UserDetails(): JSX.Element {
                 <TableRow>
                   <TableCell className={classes.texts}>Client name</TableCell>
                   <TableCell className={classes.texts}>Type</TableCell>
+                  <TableCell className={classes.texts}>Time</TableCell>
                   <TableCell className={classes.texts}>Cost</TableCell>
                   <TableCell className={classes.texts}>Paid amount</TableCell>
                   <TableCell className={classes.texts}>Labour cost</TableCell>
@@ -294,6 +207,9 @@ export default function UserDetails(): JSX.Element {
                           // @ts-ignore
                           return transactionType[item] === row.transaction_type;
                         })}
+                      </TableCell>
+                      <TableCell align='left' className={classes.texts}>
+                        {dayjs(row.timestamp).format('DD MMMM, YYYY [a]t hh:mm A')}
                       </TableCell>
                       <TableCell align="left" className={classes.texts}>
                         <NumberFormat value={row.order_cost} displayType={'text'}
